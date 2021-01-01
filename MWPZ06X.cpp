1447f9fffddb72ffee7ba17a8bc126a2fe1d98e7
Damian Tomczak <43729842+damian28102000@users.noreply.github.com>
2021-01-02 00:25:14 +0100

    Create MWPZ06X.cpp

---
+++ MWPZ06X.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int ile = 0, n=0;
+	cin >> ile;
+	for (int i =0;i<ile;i++)
+	{
+		cin >> n;
+		cout << n * n<<endl;
+	}
+
+	return 0;
+
+}
