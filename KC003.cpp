47218a8088d907f839c6fa1d779f30688383a1d2
Damian Tomczak <43729842+damian28102000@users.noreply.github.com>
2021-01-02 16:52:32 +0100

    Create KC003.cpp

---
+++ KC003.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int n1, n2, k1, k2;
+	cin>> n1 >> k1 >> n2 >> k2;
+	cout << n1 * k1 + n2 * k2;
+
+	return 0;
+
+}
